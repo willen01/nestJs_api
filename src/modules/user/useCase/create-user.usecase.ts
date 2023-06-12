1211e5ff793d96bf0ef73350cc481a39208803a0
willen <willen.sesantos@gmail.com>
2023-06-12 20:16:07 -0300

    chore: Implement the createUser use case

---
+++ src/modules/user/useCase/create-user.usecase.ts
@@ -0,0 +1,26 @@
+import { PrismaService } from "src/infra/database/prisma.service";
+
+export type CreateUserDTO = {
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+};
+
+export class CreateUserUseCase {
+  constructor(private prisma: PrismaService) {}
+
+  async execute(data: CreateUserDTO) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ username: data.username }, { email: data.email }],
+      },
+    });
+
+    if (user) {
+      throw new Error("User already exists!");
+    }
+
+    return await this.prisma.user.create({ data });
+  }
+}
